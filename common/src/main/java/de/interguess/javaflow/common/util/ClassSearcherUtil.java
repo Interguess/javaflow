1d10e353e32253d30fd15a8f44a6abc72a456603
tidyycode <tidycode@proton.me>
2024-09-23 11:44:08 +0200

    feat(common): Recoded ClassSearcherUtil

--- common/src/main/java/de/interguess/javaflow/common/util/ClassSearcherUtil.java
+++ common/src/main/java/de/interguess/javaflow/common/util/ClassSearcherUtil.java
@@ -1,10 +1,13 @@
 package de.interguess.javaflow.common.util;
 
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.reflect.ClassPath;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -13,47 +16,20 @@ import java.util.List;
 @UtilityClass
 public class ClassSearcherUtil {
 
-    public static List<Class<?>> listAllClasses(@NotNull ClassLoader classLoader, @NotNull String packageName) {
-        List<Class<?>> classes = new ArrayList<>();
-        String packagePath = packageName.replace('.', '/');
-
+    @SuppressWarnings("all") /// Supress {@link com.google.common.annotations.Beta}
+    @NotNull
+    public List<Class<?>> listAllClasses(@NotNull ClassLoader classLoader, @NotNull String packageName) {
         try {
-            Enumeration<URL> resources = classLoader.getResources(packagePath);
-            while (resources.hasMoreElements()) {
-                URL resource = resources.nextElement();
-                String filePath = resource.getFile();
+            ClassPath classPath = ClassPath.from(classLoader);
+            ImmutableList<ClassPath.ClassInfo> classInfos = classPath.getTopLevelClassesRecursive(packageName).asList();
 
-                File directory = new File(filePath);
-                if (directory.exists()) {
-                    findClasses(directory, packageName, classes);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return classes;
-    }
-
-    private static void findClasses(File directory, String packageName, List<Class<?>> classes) {
-        File[] files = directory.listFiles();
-
-        if (files == null) {
-            return;
-        }
-
-        for (File file : files) {
-            if (file.isDirectory()) {
-                findClasses(file, packageName + "." + file.getName(), classes);
-            } else if (file.getName().endsWith(".class")) {
-                try {
-                    String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
-
-                    classes.add(Class.forName(className));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+            return classInfos.stream()
+                    .map(ClassPath.ClassInfo::load)
+                    .collect(ImmutableList.toImmutableList());
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return ImmutableList.of();
         }
     }
+
 }
---
+++ common/src/test/java/de/interguess/javaflow/test/ClassSearchTest.java
@@ -0,0 +1,80 @@
+package de.interguess.javaflow.test;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.reflect.ClassPath;
+import de.interguess.javaflow.common.util.ClassSearcherUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SuppressWarnings("all")
+class ClassSearchTest {
+
+    @Mock
+    private ClassLoader mockClassLoader;
+
+    @Mock
+    private ClassPath mockClassPath;
+
+    @Mock
+    private ClassPath.ClassInfo mockClassInfo1;
+
+    @Mock
+    private ClassPath.ClassInfo mockClassInfo2;
+
+    @BeforeEach
+    void mock() throws Exception {
+        MockitoAnnotations.openMocks(this);
+
+        doReturn(String.class).when(mockClassInfo1).load();
+        doReturn(Integer.class).when(mockClassInfo2).load();
+
+        when(mockClassPath.getTopLevelClassesRecursive(anyString()))
+                .thenReturn(ImmutableSet.of(mockClassInfo1, mockClassInfo2));
+
+        try (var mockedStatic = mockStatic(ClassPath.class)) {
+            mockedStatic.when(() -> ClassPath.from(mockClassLoader)).thenReturn(mockClassPath);
+        }
+    }
+
+    @Test
+    void testSuccess() throws Exception {
+        // Test for successful class loading
+        try (var mockedStatic = mockStatic(ClassPath.class)) {
+            mockedStatic.when(() -> ClassPath.from(mockClassLoader)).thenReturn(mockClassPath);
+
+            List<Class<?>> result = ClassSearcherUtil.listAllClasses(mockClassLoader, "com.example.package");
+
+            assertNotNull(result);
+            assertEquals(2, result.size());
+            assertTrue(result.contains(String.class));
+            assertTrue(result.contains(Integer.class));
+
+            // Verify interactions
+            verify(mockClassInfo1, times(1)).load();
+            verify(mockClassInfo2, times(1)).load();
+        }
+    }
+
+    @Test
+    void testException() throws Exception {
+        // Simulate IOException in ClassPath
+        try (var mockedStatic = mockStatic(ClassPath.class)) {
+            mockedStatic.when(() -> ClassPath.from(mockClassLoader)).thenThrow(new IOException());
+
+            List<Class<?>> result = ClassSearcherUtil.listAllClasses(mockClassLoader, "com.example.package");
+            assertNotNull(result);
+            assertTrue(result.isEmpty());
+        }
+    }
+
+}
