733cca7856b10ff3413a6313f4ffecbab390b584
jonas <jonasschnell23@gmail.com>
2024-09-23 17:16:43 +0200

    chore(test): stop ignoring warnings

--- common/src/test/java/de/interguess/javaflow/test/ClassSearchTest.java
+++ common/src/test/java/de/interguess/javaflow/test/ClassSearchTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@SuppressWarnings("all")
+@SuppressWarnings("UnstableApiUsage")
 class ClassSearchTest {
 
     @Mock
